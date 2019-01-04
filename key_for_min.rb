098bfaa51a2a920c8d16dc158f6b7383996ab739
andy reyes <11710reyes@gmail.com>
2019-01-04 04:29:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(name_hash)
   low_key = nil 
   low_value = nil 
-  name_hash.each do |a, k|
-    if lowest_value == nil || k < low_value
-      low_value = k 
-      low_key = a 
+  name_hash.each do |k, v|
+    if lowest_value == nil || v < low_value
+      low_value = v 
+      low_key = k 
     end 
   end 
   low_key
